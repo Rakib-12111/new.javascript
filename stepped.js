3d3163fcf2354398b9c30d94cd9ed5dd3463a379
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-08-30 16:09:20 +0600

    new file added finishes

--- stepped.js
+++ stepped.js
@@ -1,7 +1,21 @@
+// const price = 5000;
+// if(price >= 5000){
+//     // 10% discount
+//     const discount = price * 10 /100;
+//     const payAmount = price -discount;
+//     console.log(payAmount)
+// }
+
 const price = 5000;
 if(price >= 5000){
     // 10% discount
-    const discount = price * 10 /100;
-    const payAmount = price -discount;
+    const discount =price * 10 /100;
+    console.log(discount)
+}
+
+const prices = 6000;
+if(prices >= 6000){
+    const discount = prices * 10 / 100;
+    const payAmount = prices - discount;
     console.log(payAmount)
 }

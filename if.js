fc1cc46519613a3db8a895f52bcc63563bdd4dba
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-08-30 08:57:51 +0600

    third commit

---
+++ if.js
@@ -0,0 +1,42 @@
+// if ( 3 < 10){
+
+//    console.log("i have a soaller value") 
+// }
+
+// if ( 10 < 20){
+//     console.log('I have big Number ')
+// }
+
+// if ( 10 > 20){
+//     console.log("I have big number and big")
+// }
+
+// if( 20 > 10){
+//     console.log( " ami holam 20 number ")
+// }
+
+//---------------------------------------------------------
+
+var biryaniPrice = 500;
+
+
+
+
+if (biryaniPrice < 300){
+console.log('mama give me some brryani')
+}
+
+
+
+var  weight = 20;
+if(weight < 20){
+    console.log('I will carry it by myself');
+
+
+}
+
+else{
+    console.log("I will   rent a rickshaw")
+}
+
+
---
+++ multiple.js
@@ -0,0 +1,21 @@
+// const salary = 25000;
+// const isBCS = true;
+// const height = 61;
+// if (salary > 20000 && height > 66 ){
+//     console.log('su---patro')
+// }
+
+// else{
+//     console.log('onno patro khuji')
+// }
+const salary = 25000;
+const isBCS = true;
+const height = 61;
+if(salary > 20000 || height > 66){
+    console.log('su-----patro')
+
+}
+
+else{
+    console.log('onno patro khuji')
+}

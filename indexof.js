5f7e41760c7aea3a5ce30d2b940facf2040460df
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-08-30 06:51:49 +0600

    first commit

---
+++ indexof.js
@@ -0,0 +1,30 @@
+// let unm = ' hello world '
+// console.log(unm)
+// let sum = 'hello world hello fornnd'
+
+// let text5 = `Hello "Hablu Programmers"`
+// console.log(text5)
+
+let firstName = "Rakib";
+let lastName = "Ahmed Rakib";
+console.log(`my name is ${firstName}${lastName}`);
+
+
+
+
+let sum= 20;
+let sum2= 30;
+console.log(`This is javascript expressions: ${sum+sum2} `)
+
+let sum3 = ' hello world'
+let sum4 = ' hello bangladesh'
+console.log(`this is my writing heree${sum3+sum4}`)
+
+
+
+
+
+
+
+
+

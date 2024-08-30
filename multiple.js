24dfab000612382d5baa1f05afa7306a37c6c635
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-08-30 15:53:43 +0600

    new file added

--- multiple.js
+++ multiple.js
@@ -8,14 +8,23 @@
 // else{
 //     console.log('onno patro khuji')
 // }
-const salary = 25000;
+const salary = 25001;
 const isBCS = true;
 const height = 61;
-if(salary > 20000 || height > 66){
-    console.log('su-----patro')
+// if(salary > 20000 || height > 66){
+//     console.log('su-----patro')
 
+// }
+
+// else{
+//     console.log('onno patro khuji')
+// }
+
+
+if(salary > 25000 || height > 72){
+    console.log('eso baba kobul');
 }
 
 else{
-    console.log('onno patro khuji')
+    console.log('vak tui mokbul')
 }
